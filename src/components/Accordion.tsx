f073279a5e6956e0891610504658b2da9efcc5be
Diana Llona <dev.dianallona@gmail.com>
2023-10-24 10:37:16 +0800

    fix accordion

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -17,19 +17,12 @@ const Accordion: FC<
       )}
       {...otherProps}
     >
-      <div className='w-6 h-6 flex justify-center mr-2'>
-        <input
-          type='checkbox'
-          id={id}
-          className='peer appearance-none'
-          hidden
-        />
-        <i className='fa-solid fa-chevron-right peer-checked:hidden flex items-center'></i>
-        <i className='fa-solid fa-chevron-down peer-checked:flex items-center hidden'></i>
-      </div>
+      <input type='checkbox' id={id} className='peer appearance-none' hidden />
+      <i className='fa-solid fa-chevron-right peer-checked:hidden flex items-center justify-center w-6 h-6'></i>
+      <i className='fa-solid fa-chevron-down peer-checked:flex items-center hidden justify-center w-6 h-6'></i>
       <label
         htmlFor={id}
-        className='items-center cursor-pointer grow flex justify-between'
+        className='items-center cursor-pointer grow flex justify-between pl-2'
       >
         <p className='text-[#2D2D2D] text-s font-semibold leading-4'>{title}</p>
         <p className='text-primary/75 bg-success px-2 p-1 text-center text-xxs font-medium leading-normal rounded-xs'>
