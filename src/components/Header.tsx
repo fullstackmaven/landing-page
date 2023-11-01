c91a83734a734cd381dcc55468cdd8e8c2954248
Diana Llona <dev.dianallona@gmail.com>
2023-11-01 10:18:32 +0800

    fix header lagging

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export const Header: FC<THeaderProps & HTMLAttributes<HTMLDivElement>> = ({
 
   const handleScroll = () => {
     // Handle the scroll event here
-    const threshold = 88; // Adjust this value as needed
+    const threshold = 104; // Adjust this value as needed
     if (window.scrollY <= threshold) {
       setIsScrollAtTop(true);
     } else {
@@ -40,64 +40,68 @@ export const Header: FC<THeaderProps & HTMLAttributes<HTMLDivElement>> = ({
 
   return (
     <div
-      className={classNames(
-        'sticky top-0 w-full flex items-center px-2 py-6 md:px-8 lg:px-4 z-10 transition-all duration-150 ease-in',
-        className,
-        {
-          'bg-transparent md:!w-[calc(100%-16px)] !py-3 md:bg-white rounded-l md:shadow-md top-0 md:top-2 !left-2 w-fit':
-            !isScrollAtTop,
-        },
-      )}
+      className={classNames('sticky top-0 z-10', className, {
+        'bg-transparent md:px-2 md:pt-8': !isScrollAtTop,
+      })}
       {...otherProps}
     >
       <div
         className={classNames(
-          'flex-1 lg:flex-none text-primary text-l font-extrabold',
+          'w-full flex items-center px-2 py-6 md:px-8 lg:px-4 transition-all duration-150 ease-in',
           {
-            'opacity-0 md:opacity-100': !isScrollAtTop,
+            'bg-transparent rounded-l md:bg-white md:shadow-md': !isScrollAtTop,
           },
         )}
       >
-        Cyram
-      </div>
-      <nav className='hidden lg:flex-1 md:justify-center lg:flex'>
-        <ul className='flex justify-between items-center lg:gap-3'>
-          {menuItems.map((menuItem, index) => (
-            <li
-              key={`header-${index}`}
-              className='lg:cursor-pointer lg:hover:bg-[#EBEBEB] hover:rounded-s transition duration-150 ease-out hover:ease-in'
-            >
-              <a
-                href={`#${menuItem.id}`}
-                className='px-3 py-2 w-full flex justify-between items-center gap-1'
-              >
-                <p className='text-primary text-s leading-[21px] font-medium'>
-                  {menuItem.label}
-                </p>
-                {/* <img src='home-page/show-more.svg' alt='dropdown-icon' /> */}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className='flex justify-between items-center gap-4'>
-        <Button
-          title='Book a call'
-          className={classNames('!h-[35px] !text-s font-medium', {
-            'hidden md:flex': !isScrollAtTop,
-          })}
-        />
-        <button
-          className={classNames('py-2 px-1 h-[35px]', {
-            'bg-white md:bg-transparent rounded-xs': !isScrollAtTop,
-          })}
+        <div
+          className={classNames(
+            'flex-1 lg:flex-none text-primary text-l font-extrabold',
+            {
+              'opacity-0 md:opacity-100': !isScrollAtTop,
+            },
+          )}
         >
-          <img
-            src='hamburger.svg'
-            alt='mobile-menu-icon'
-            className='w-6 h-4 lg:hidden'
+          Cyram
+        </div>
+        <nav className='hidden lg:flex-1 md:justify-center lg:flex'>
+          <ul className='flex justify-between items-center lg:gap-3'>
+            {menuItems.map((menuItem, index) => (
+              <li
+                key={`header-${index}`}
+                className='lg:cursor-pointer lg:hover:bg-[#EBEBEB] hover:rounded-s transition duration-150 ease-out hover:ease-in'
+              >
+                <a
+                  href={`#${menuItem.id}`}
+                  className='px-3 py-2 w-full flex justify-between items-center gap-1'
+                >
+                  <p className='text-primary text-s leading-[21px] font-medium'>
+                    {menuItem.label}
+                  </p>
+                  {/* <img src='home-page/show-more.svg' alt='dropdown-icon' /> */}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className='flex justify-between items-center gap-4'>
+          <Button
+            title='Book a call'
+            className={classNames('!h-[35px] !text-s font-medium', {
+              'hidden md:flex': !isScrollAtTop,
+            })}
           />
-        </button>
+          <button
+            className={classNames('py-2 px-1 h-[35px]', {
+              'bg-white md:bg-transparent rounded-xs': !isScrollAtTop,
+            })}
+          >
+            <img
+              src='hamburger.svg'
+              alt='mobile-menu-icon'
+              className='w-6 h-4 lg:hidden'
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
