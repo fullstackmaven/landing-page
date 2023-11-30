45ee5f59cbcf88dac1d0c332f5677708127bf069
Diana Llona <dev.dianallona@gmail.com>
2023-11-30 21:08:41 +0800

    rever contents

--- src/components/sections/qaCourse/curriculum/content/Content.tsx
+++ src/components/sections/qaCourse/curriculum/content/Content.tsx
@@ -6,14 +6,8 @@ type TContentProps = Pick<TCurriculum, 'topics' | 'description'>;
 const Content: FC<TContentProps> = ({ description, topics }) => {
   return (
     <div className='h-full w-full space-y-4 pb-3 text-primary'>
-      <h1 className='text-xl font-bold text-primary'>
-        Software Testing Fundamentals
-      </h1>
-      <p className='text-base font-normal tablet:text-s laptop:py-6 laptop:text-base laptop:tracking-widest'>
+      <p className='text-xs font-normal tablet:text-s laptop:py-6 laptop:text-base'>
         {description}
-      </p>
-      <p className='text-base font-semibold text-primary'>
-        What you'll focus on
       </p>
       <ul className='grid list-inside list-disc gap-y-2 text-xs font-normal text-primary tablet:text-s laptop:text-s'>
         {topics.map((topic, index) => (
